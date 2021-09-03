e7f6deb94a68c3c34d9388d0ec64cdc9800daa7e
liaokongVFX <568250549@qq.com>
2021-09-03 23:34:46 +0800

    完善了详情页的样式

--- src/pages/detail/$id$.js
+++ src/pages/detail/$id$.js
@@ -3,8 +3,8 @@ import {connect} from 'dva'
 import {Card, Row, Col, Switch, DatePicker, Input} from 'antd'
 import moment from 'moment';
 
-
 import {Content} from "@/components/Layout";
+import styles from './index.scss'
 
 const {TextArea} = Input
 
@@ -18,39 +18,39 @@ class $Id extends Component {
         <Content>
           <Row gutter={16}>
             <Col span={6}>
-              <Card>
-                <p>运行事件数</p>
-                <p>8</p>
+              <Card className={styles.number}>
+                <p className={styles.title}>运行事件数</p>
+                <p className={`${styles.text} ${styles.run}`}>8</p>
               </Card>
             </Col>
             <Col span={6}>
-              <Card>
-                <p>暂停事件数</p>
-                <p>2</p>
+              <Card className={styles.number}>
+                <p className={styles.title}>暂停事件数</p>
+                <p className={`${styles.text} ${styles.stop}`}>2</p>
               </Card>
             </Col>
             <Col span={6}>
-              <Card>
-                <p>日志警告数</p>
-                <p>2</p>
+              <Card className={styles.number}>
+                <p className={styles.title}>日志警告数</p>
+                <p className={`${styles.text} ${styles.warning}`}>2</p>
               </Card>
             </Col>
             <Col span={6}>
-              <Card>
-                <p>日志错误数</p>
-                <p>12</p>
+              <Card className={styles.number}>
+                <p className={styles.title}>日志错误数</p>
+                <p className={`${styles.text} ${styles.error}`}>12</p>
               </Card>
             </Col>
           </Row>
           <Row>
             <Col span={2} style={colStyle}>
-              <p>日志详情：</p>
+              <p style={{fontSize: 15}}>日志详情：</p>
             </Col>
-            <Col span={16}>
+            <Col span={15}>
             </Col>
-            <Col span={3} style={{marginTop: 25}}>
+            <Col span={4} style={{marginTop: 25}}>
               <p style={{display: "flex", alignItems: "center"}}>
-                <span style={{marginRight: 10}}>只显示错误日志</span>
+                <span style={{marginRight: 12, fontSize: 15}}>只显示错误日志</span>
                 <Switch checkedChildren="开启" unCheckedChildren="关闭"/></p>
             </Col>
             <Col span={3} style={colStyle}>
@@ -61,7 +61,7 @@ class $Id extends Component {
           <Row>
             <Col>
               <TextArea
-                style={{height: 560, marginTop: 10, resize: "none"}}
+                style={{height: 580, marginTop: 10, resize: "none"}}
                 readonly="readonly"/>
             </Col>
           </Row>
